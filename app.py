b1ce5945fda0e2c2f7bfa4f0876454c47702cf7b
syn4z <iatcosorin797@gmail.com>
2024-02-29 18:39:19 +0200

    refactor: change app, add Procfile

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def hello():
+    return 'Hello, Azure App Service!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
