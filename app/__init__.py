9bda4b85dc79cb0c5171b3319d89b68b17a6edd3
Grena30 <grenadinu@yahoo.com>
2024-03-12 20:45:31 +0200

    fix: change connection string

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template
 from flask_restful import Api
-from .api.routes import upload_image, get_image, delete_image, update_image
+# from .api.routes import *
 
 app = Flask(__name__)
 api = Api(app, prefix='/api')
-api.add_resource(upload_image, '/upload')
-api.add_resource(get_image, '/image/<filename>')
-api.add_resource(delete_image, '/image/<filename>')
-api.add_resource(update_image, '/image/<filename>')
+# api.add_resource(upload_image, '/upload')
+# api.add_resource(get_image, '/image/<filename>')
+# api.add_resource(delete_image, '/image/<filename>')
+# api.add_resource(update_image, '/image/<filename>')
 
 @app.route('/')
 def index():
