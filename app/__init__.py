54d0adfb6b67de55554d6d73f3aaf17b33c40983
syn4z <iatcosorin797@gmail.com>
2024-03-16 17:33:18 +0200

    chore: test api with CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-from flask_restful import Api
-from .api.routes import *
+# from flask_restful import Api
+# from .api.routes import *
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app, prefix='/api')
+CORS(app)
+# api = Api(app, prefix='/api')
 # api.add_resource(upload_image, '/upload')
 # api.add_resource(get_image, '/image/<filename>')
 # api.add_resource(delete_image, '/image/<filename>')
@@ -13,6 +15,6 @@ api = Api(app, prefix='/api')
 def index():
     return render_template('index.html')
 
-@app.route('/upload')
+@app.route('/test', methods=['GET'])
 def upload():
     return {"This": "works"}
